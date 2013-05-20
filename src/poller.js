ce8fa576490bbbb4571c7598ae4e84204a2c422b
Emil Wall <emil.wall@valtech.se>
2013-05-20 17:33:53 +0200

    Check that url is defined when starting polling

--- src/poller.js
+++ src/poller.js
@@ -2,6 +2,9 @@
   var ajax = tddjs.namespace("ajax");
 
   function start() {
+    if (!this.url) {
+      throw new TypeError("Must specify URL to poll");
+    }
   }
 
   ajax.poller = {
--- test/poller_test.js
+++ test/poller_test.js
@@ -9,6 +9,15 @@
     "test should define a start method":
     function () {
       assertFunction(ajax.poller.start);
+    },
+
+    "test start should throw exception for missing URL":
+    function () {
+      var poller = Object.create(ajax.poller);
+
+      assertException(function () {
+        poller.start();
+      }, "TypeError");
     }
   });
 }());
