a4c3b11239070c34a4bd3d986d5f478ea15b8f36
Emil Wall <emil.wall@valtech.se>
2013-05-21 07:47:17 +0200

    Call request with url
    Somehow I was able to get the test to pass without implementing the functionality,
    but only before restarting the test driver. Tests seems to work as intended now.

--- test/poller_test.js
+++ test/poller_test.js
@@ -2,6 +2,16 @@
   var ajax = tddjs.ajax;
 
   TestCase("PollerTest", {
+    setUp: function () {
+      this.ajaxCreate = ajax.create;
+      this.xhr = Object.create(fakeXMLHttpRequest);
+      ajax.create = stubFn(this.xhr);
+    },
+
+    tearDown: function () {
+      ajax.create = this.ajaxCreate;
+    },
+
     "test should be object": function () {
       assertObject(ajax.poller);
     },
@@ -18,6 +28,16 @@
       assertException(function () {
         poller.start();
       }, "TypeError");
+    },
+
+    "test start should make XHR request with URL": function () {
+      var poller = Object.create(ajax.poller);
+      poller.url = "/url";
+
+      poller.start();
+
+      assert(this.xhr.open.called);
+      assertEquals(poller.url, this.xhr.open.args[1]);
     }
   });
 }());
