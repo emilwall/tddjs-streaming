11261bc8351d4d9c301ce1dbd73009e4b2f5bbce
Emil Wall <emil.wall@valtech.se>
2013-05-23 13:37:56 +0200

    Alternate date implementations between calls to constructor
    At least once.

--- test/date_test.js
+++ test/date_test.js
@@ -2,6 +2,14 @@
   var ajax = tddjs.ajax;
 
   TestCase("DateTest", {
+    setUp: function () {
+      this.date = Date;
+    },
+
+    tearDown: function () {
+      Date = this.date;
+    },
+
     "test should define stubDateConstructor": function () {
       assertFunction(stubDateConstructor);
     },
@@ -12,6 +20,15 @@
       var date2 = new Date();
 
       assertSame(date1, date2);
-    }
+    },
+
+    "test should return new Date every other time": function () {
+      var date1 = new Date();
+      stubDateConstructor(date1);
+      var date2 = new Date();
+      var date3 = new Date();
+
+      assertNotSame(date1, date3);
+    },
   });
 }());
