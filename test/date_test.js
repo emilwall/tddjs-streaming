737daa954e2b497f5f6c23a55e635cecea616c27
Emil Wall <emil.wall@valtech.se>
2013-05-23 10:47:38 +0200

    Enable stubbing of Date to return same object twice

--- test/date_test.js
+++ test/date_test.js
@@ -4,6 +4,14 @@
   TestCase("DateTest", {
     "test should define stubDateConstructor": function () {
       assertFunction(stubDateConstructor);
+    },
+
+    "test should make Date constructor return same object twice": function () {
+      var date1 = new Date();
+      stubDateConstructor(date1);
+      var date2 = new Date();
+
+      assertSame(date1, date2);
     }
   });
 }());
